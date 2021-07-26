923cccc181c342e072a6e2b438e01f9a45af09dd
Andris Reinman <andris@kreata.ee>
2021-07-26 11:31:28 +0300

    added example dkim signature

--- plugins/example-dkim.js
+++ plugins/example-dkim.js
@@ -5,6 +5,16 @@
 // DNS entry
 // test._domainkey.example.com:v=DKIM1;p=MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDVb7eRJNfQrfnEr6GyZaRq1JxbxQh5+h+4D2OseH2t/vii+8OXvkpApTUEO2woJ1tbeNSGinnxfWnxZpoZaCjrhqJHrtFR6pQFcD/FnT92w5eyru+kq5yLAv+IXlZXvJXoOw9gN3NwBJ6p+EqC1/1hsOl4dedDFd/xmMlHk6wj8wIDAQAB
 
+// Sent message should have a signature like the following:
+/*
+DKIM-Signature: v=1; a=rsa-sha256; c=relaxed/relaxed; d=example.com;
+ q=dns/txt; s=test; bh=VRtPFKW6BFIbUeboSJtHw3z5AVJGV/i9RPj/ISlIzAk=;
+ h=subject:date:message-id;
+ b=BQsM8Qso5gSYpBHOCb2MVc0cW5QRnF4upuIWfE4CcpQHIPKuxJF+u4ToFKvkhnqAqqOHMcB5z
+ V3Vr4Loj7ozCtn/rfSXiyY8sOoaYqqcUBgjuKdJ0gUNySb4ADKfgfyhREf117c6OjajQRenMO9r
+ +rdBEDvBOD9Bxc2KzVUhyfE=
+*/
+
 const DKIM_DOMAIN_NAME = 'example.com';
 const DKIM_SELECTOR = 'test';
 const DKIM_KEY = `-----BEGIN RSA PRIVATE KEY-----
